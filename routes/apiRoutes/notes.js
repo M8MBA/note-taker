bf6d5ddee5a6a4c4f85119ca856f9b609250d812
M8MBA <sauce.ix@gmail.com>
2022-04-05 14:12:39 -0600

    unique id npm

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,14 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-
-const { notes } = require('')
+const router = express.Router();
+// create a uuid to give each note a unique id (npm)
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 // get all notes
-app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+route.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
   })
 });
 
 // create a new note
+router.post('api/notes', (req, res) => {
+  const newNote = {
+    title:request.body.title,
+    text: request.body.text,
+    id: uuidv4()
+  }
+
+  return fs.readFile('db/db.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+  
+    fs.writeFile('db/db.json', JSON.stringify(notes), () => {
+      response.json(true);
+    })
+
+  })
+
+})
+
+module.exports = router;
