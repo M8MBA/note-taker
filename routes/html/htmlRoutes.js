717151bc253dec9eed98438959d9ed786404de2f
M8MBA <sauce.ix@gmail.com>
2022-04-05 18:09:27 -0600

    __dirname define

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -5,13 +5,17 @@ const router = require('express').Router();
 
 // retrieve main page
 router.get ('/', (req, res) => {
-  res.sendFile(path.join(_dirname, '../../public/index.html'))
-})
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 // retrieve notes page
 router.get ('/notes', (req, res) => {
-  res.sendFile(path.join(_dirname, '../../public/notes.html'))
-})
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 module.exports = router;
 
